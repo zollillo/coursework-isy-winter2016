69f570414392896356672ce18ce7866aa1dba04a
Nadja Zollo <nadja.zollo@gmail.com>
2016-10-26 21:16:07 +0200

    Define matrix and column vectors using numpy. Resolves task #1.1.

--- 01-basics/01_basics.py
+++ 01-basics/01_basics.py
@@ -17,6 +17,35 @@ from gaussian_kernel_calculator import *
 # A1. Numpy and Linear Algebra
 
 # (1) Define Matrix M and column vectors vec0 ... vec3 in numpy
+M = np.array([[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9],
+              [0, 2, 2]], dtype='f')
+vec0 = np.array([1, 1, 0], dtype='f').reshape(3, 1)
+vec1 = np.array([-1, 2, 5], dtype='f').reshape(3, 1)
+vec2 = np.array([1, 2, 3, 4, 5], dtype='f').reshape(5, 1)
+vec3 = np.array([-1, 9, 5, 3, 1], dtype='f').reshape(5, 1)
+
+# Print to console to check the results
+print 'M ='
+print M
+print 'Shape of M is', M.shape, '\n'
+
+print 'vec0 ='
+print vec0
+print 'Shape of vec0 is', vec0.shape, '\n'
+
+print 'vec1 ='
+print vec1
+print 'Shape of vec1 is', vec1.shape, '\n'
+
+print 'vec2='
+print vec2
+print 'Shape of vec2 is', vec2.shape, '\n'
+
+print 'vec3='
+print vec3
+print 'Shape of vec3 is', vec3.shape, '\n'
 
 
 # (2) Please implement a dot product and magnitude function for arbitrary vectors
