f56a3c241cd73731d5a24298b64b9eac0e2cb987
Nadja Zollo <nadja.zollo@gmail.com>
2016-10-28 19:52:57 +0200

    Adjust pattern size. Rename variables. Print values to console.

--- 01-basics/01_camera_calibration.py
+++ 01-basics/01_camera_calibration.py
@@ -15,12 +15,13 @@ import glob
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6 * 7, 3), np.float32)
-objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
+pattern_size = (7, 7)
+pattern_points = np.zeros((np.product(pattern_size), 3), np.float32)
+pattern_points[:, :2] = np.indices(pattern_size).T.reshape(-1, 2)
 
 # Arrays to store object points and image points from all the images.
-objpoints = []  # 3d point in real world space
-imgpoints = []  # 2d points in image plane.
+obj_points = []  # 3d point in real world space
+img_points = []  # 2d points in image plane.
 
 
 cap = cv2.VideoCapture(0)
@@ -40,13 +41,37 @@ while True:
 
     # Wait for 'c' to use current frame as a test pattern for camera calibration.
     if k == ord('c'):
-        print 'Pressed c'
         # Find the chessboard corners
-        found, corners = cv2.findChessboardCorners(gray, (7, 6), None)
+        found, corners = cv2.findChessboardCorners(gray, pattern_size, None)
 
-        # If found, add object points, image points (after refining them)
+        # Show info if no chessboard corners were found.
+        if not found:
+            print('Chessboard not found!')
+            continue
+
+        # If found, add object points, image points (after refining them).
         if found:
-            print 'Chessboard found'
+            print 'Chessboard found!'
+            obj_points.append(pattern_points)
+
+            corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+            img_points.append(corners2)
+
+            # Draw and display the corners
+            img = cv2.drawChessboardCorners(frame, pattern_size, corners2, found)
+            cv2.imshow('img', img)
+
+            # Calculate camera distortion
+            rms, mtx, dist_coefs, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, gray.shape[::-1], None,
+                                                                     None)
+
+            print '\nroot mean square (RMS) projection error:', rms
+            print 'camera matrix:\n', mtx
+            print 'distortion coefficients:', dist_coefs.ravel()
+
+    # Wait for 'q' to exit
+    elif k == ord('q'):
+        break
 #     # Find the chess board corners
 #     ret, corners = cv2.findChessboardCorners(gray, (7, 6), None)
 #
